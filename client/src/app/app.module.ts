264a319fbed1b6a1e8d2a02baeb3d5af1863f0d0
Melani Mikela <41188245+melanisilva@users.noreply.github.com>
2020-02-28 02:06:19 +0530

    chore(data-science): add variable prediction logic
    * Data Science part for variables
    
    * refactor : change gitignore
    
    * test
    
    * test
    
    * test2
    
    * test
    
    * chore(deps): remove mistakenly tracked node modules
    
    Co-authored-by: ChamikaWickramaratne <56549417+ChamikaWickramaratne@users.noreply.github.com>
    Co-authored-by: Zuha Mursheed <57676563+zuhamursheed@users.noreply.github.com>
    Co-authored-by: RizkhanRazik <56043047+RizkhanRazik@users.noreply.github.com>

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkshopComponent } from './pages/workshop/workshop.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkshopComponent,
+    StatisticsComponent
   ],
   imports: [
     BrowserModule,
