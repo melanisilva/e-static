9d98137474b76e5df1f2794e6dbc5ad24927e9ed
zuhamursheed <57676563+zuhamursheed@users.noreply.github.com>
2020-02-27 10:40:29 +0530

    test

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WorkshopComponent } from './pages/workshop/workshop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WorkshopComponent
   ],
   imports: [
     BrowserModule,
