d3e95cf3b3a64540cabadc7e91e364574fdc2a09
zuhamursheed <57676563+zuhamursheed@users.noreply.github.com>
2020-04-01 16:16:46 +0530

    UI Changes

--- client/src/app/pages/workshop/workshop.module.ts
+++ client/src/app/pages/workshop/workshop.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WorkshopComponent } from './../../pages/workshop/workshop.component';
+import { AddComponent } from './add/add.component';
+import { DeleteComponent } from './delete/delete.component';
+import { UpdateComponent } from './update/update.component';
+
 
 @NgModule({
-  declarations: [
-   
-    WorkshopComponent,
-  
-  ],
-  imports: [
-    RouterModule,
-  ]
+  imports: [RouterModule.forRoot([
+    {path:'add',
+    component :AddComponent},
+   {path:'delete',
+    component :DeleteComponent},
+   {path:'update',
+    component :UpdateComponent},
+  ])],
+
+
+exports: [RouterModule]
 })
+
 export class WorkshopModule { }
