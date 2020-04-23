60c3a0a632b469148a1396b2a7383190d2c36458
zuhamursheed <57676563+zuhamursheed@users.noreply.github.com>
2020-04-23 19:34:51 +0530

    UI Changes

--- client/src/app/pages/workshop/add/add.component.ts
+++ client/src/app/pages/workshop/add/add.component.ts
@@ -3,10 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
+  template:'<input [(ngModel)] ="name" type ="text"> {{name}} ',
+styles :[]
+  
 })
 export class AddComponent implements OnInit {
-
+public name="";
   constructor() { }
 
   ngOnInit(): void {
