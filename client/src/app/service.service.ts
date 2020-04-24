aa6a1ab0d97cb89328a1db67c6e23fd08aa9279b
KshemendraJayathilaka <kshemendra.2018180@iit.ac.lk>
2020-04-24 12:45:25 +0530

    api set up for the add and update pages

--- client/src/app/service.service.ts
+++ client/src/app/service.service.ts
@@ -3,8 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Pets } from './pages/workshop/view/view.component';
 
 
-
-
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +11,13 @@ export class DataServiceService {
   constructor(private http: HttpClient) { }
   retrievepets(){
     return this.http.get<Pets[]>('http://localhost:8080/pets/')}
+
+  addData(adding:object){
+    return this.http.post<any>("http://localhost:8080/add/",adding)
+  }
+  updateData(updating:object){
+    return this.http.post<any>("http://localhost:8080/update/",updating)
+  }
 
 
   }
