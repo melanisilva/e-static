270cdd4cea79de6a1d6c867408b259dcbc2e4e0b
KshemendraJayathilaka <kshemendra.2018180@iit.ac.lk>
2020-04-26 08:53:21 +0530

    finishing off validation of add and update page

--- client/src/app/pages/workshop/add/add.component.ts
+++ client/src/app/pages/workshop/add/add.component.ts
@@ -11,41 +11,113 @@ import { DataServiceService } from 'src/app/service.service';
 })
 export class AddComponent implements OnInit {
   public name="";
+
+
+  public mainError = '<p style= "color: red;">an error has occored</p>';
+  public error = '<p style= "color: red;">an error has occored</p>';
+
+
+
   constructor(private service:DataServiceService) { }
 
   ngOnInit(): void {
   }
 
-  passAdd(){
-    let addYear = (<HTMLInputElement>document.getElementById('addYear')).value;
-    let addPopulation = (<HTMLInputElement>document.getElementById('addPopulation')).value;
-    let addGDP1 = (<HTMLInputElement>document.getElementById('addGDP1')).value;
-    let addDom = (<HTMLInputElement>document.getElementById('addDom')).value;
-    let addGDP2 = (<HTMLInputElement>document.getElementById('addGDP2')).value;
-    let addGDP3 = (<HTMLInputElement>document.getElementById('addGDP3')).value;
-    let addAvg = (<HTMLInputElement>document.getElementById('addAvg')).value;
 
-    var adding ={
-      addYear:addYear,
-      addPopulation:addPopulation,
-      addGDP1:addGDP1,
-      addDom:addDom,
-      addGDP2:addGDP2,
-      addGDP3:addGDP3,
-      addAvg:addAvg
-    };
-
-    this.service.addData(adding).subscribe(
-      (data)=>{
-        console.log(data);
-      }
+  errorControl(){
+    var addPopulation = document.getElementById("addPopulation");
+    var addGDP1 = document.getElementById("addGDP1");
+    var addDom = document.getElementById("addDom");
+    var addGDP2 = document.getElementById("addGDP2");
+    var addGDP3 = document.getElementById("addGDP3");
+    var addAvg = document.getElementById("addAvg");
 
 
+    if(typeof(addPopulation) == 'number'){
+
+       if(typeof(addGDP1) == 'number'){
+
+        if(typeof(addDom) == 'number'){
+
+          if(typeof(addGDP2) == 'number'){
+
+            if(typeof(addGDP3) == 'number'){
+
+              if(typeof(addAvg) == 'number'){
+                return true;
 
 
+                }else{
+                  document.getElementById("error6").innerHTML = this.error;
+                  return false;
+                }
+            
 
-    )
 
+              }else{
+                document.getElementById("error5").innerHTML = this.error;
+                return false;
+              }
+            
+
+
+            }else{
+              document.getElementById("error4").innerHTML = this.error;
+              return false;
+            }
+            
+
+
+          }else{
+            document.getElementById("error3").innerHTML = this.error;
+            return false;
+          }
+
+        }else{
+          document.getElementById("error2").innerHTML = this.error;
+          return false;
+        }
+
+      
+    }else{
+      document.getElementById("error1").innerHTML = this.error;
+      return false;
+    }
   }
+
+
+
+  passAdd(){
+
+    if(this.errorControl){
+      let addYear = (<HTMLInputElement>document.getElementById('addYear')).value;
+      let addPopulation = (<HTMLInputElement>document.getElementById('addPopulation')).value;
+      let addGDP1 = (<HTMLInputElement>document.getElementById('addGDP1')).value;
+      let addDom = (<HTMLInputElement>document.getElementById('addDom')).value;
+      let addGDP2 = (<HTMLInputElement>document.getElementById('addGDP2')).value;
+      let addGDP3 = (<HTMLInputElement>document.getElementById('addGDP3')).value;
+      let addAvg = (<HTMLInputElement>document.getElementById('addAvg')).value;
+  
+      var adding ={
+        addYear:addYear,
+        addPopulation:addPopulation,
+        addGDP1:addGDP1,
+        addDom:addDom,
+        addGDP2:addGDP2,
+        addGDP3:addGDP3,
+        addAvg:addAvg
+      };
+  
+      this.service.addData(adding).subscribe(
+        (data)=>{
+          console.log(data);
+        }
+      )
+    }else{
+      document.getElementById("Mainerror").innerHTML = this.mainError;
+    }
+
+    }
+
 
 }
--- client/src/app/pages/workshop/update/update.component.ts
+++ client/src/app/pages/workshop/update/update.component.ts
@@ -9,35 +9,118 @@ import { DataServiceService } from 'src/app/service.service';
 })
 export class UpdateComponent implements OnInit {
 
+
+  // error massages
+  public mainError = '<p style= "color: red;">an error has occored</p>';
+  public error = '<p style= "color: red;">an error has occored</p>';
+
   constructor(private service:DataServiceService) { }
 
   ngOnInit(): void {
   }
 
+
+  errorControl(){
+    var updatePopulation = document.getElementById("updatePopulation");
+    var updateGDP1 = document.getElementById("updateGDP1");
+    var updateDom = document.getElementById("updateDom");
+    var updateGDP2 = document.getElementById("updateGDP2");
+    var updateGDP3 = document.getElementById("updateGDP3");
+    var updateAvg = updateAvg.getElementById("addAvg");
+
+
+    if(typeof(updatePopulation) == 'number'){
+
+       if(typeof(updateGDP1) == 'number'){
+
+        if(typeof(updateDom) == 'number'){
+
+          if(typeof(updateGDP2) == 'number'){
+
+            if(typeof(updateGDP3) == 'number'){
+
+              if(typeof(updateAvg) == 'number'){
+                return true;
+
+
+                }else{
+                  document.getElementById("error6").innerHTML = this.error;
+                  return false;
+                }
+            
+
+
+              }else{
+                document.getElementById("error5").innerHTML = this.error;
+                return false;
+              }
+            
+
+
+            }else{
+              document.getElementById("error4").innerHTML = this.error;
+              return false;
+            }
+            
+
+
+          }else{
+            document.getElementById("error3").innerHTML = this.error;
+            return false;
+          }
+
+        }else{
+          document.getElementById("error2").innerHTML = this.error;
+          return false;
+        }
+
+      
+    }else{
+      document.getElementById("error1").innerHTML = this.error;
+      return false;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
   passUpdate(){
-    let year = (<HTMLInputElement>document.getElementById('year')).value;
-    let updatePopulation = (<HTMLInputElement>document.getElementById('updatePopulation')).value;
-    let updateGDP1 = (<HTMLInputElement>document.getElementById('updateGDP1')).value;
-    let updateDom = (<HTMLInputElement>document.getElementById('updateDom')).value;
-    let updateGDP2 = (<HTMLInputElement>document.getElementById('updateGDP2')).value;
-    let updateGDP3 = (<HTMLInputElement>document.getElementById('updateGDP3')).value;
-    let updateAvg = (<HTMLInputElement>document.getElementById('updateAvg')).value;
+      if(this.errorControl){
+          
+        let year = (<HTMLInputElement>document.getElementById('year')).value;
+        let updatePopulation = (<HTMLInputElement>document.getElementById('updatePopulation')).value;
+        let updateGDP1 = (<HTMLInputElement>document.getElementById('updateGDP1')).value;
+        let updateDom = (<HTMLInputElement>document.getElementById('updateDom')).value;
+        let updateGDP2 = (<HTMLInputElement>document.getElementById('updateGDP2')).value;
+        let updateGDP3 = (<HTMLInputElement>document.getElementById('updateGDP3')).value;
+        let updateAvg = (<HTMLInputElement>document.getElementById('updateAvg')).value;
 
-    var updating ={
-      year:year,
-      updatePopulation:updatePopulation,
-      updateGDP1:updateGDP1,
-      updateDom:updateDom,
-      updateGDP2:updateGDP2,
-      updateGDP3:updateGDP3,
-      updateAvg:updateAvg
-    };
+        var updating ={
+          year:year,
+          updatePopulation:updatePopulation,
+          updateGDP1:updateGDP1,
+          updateDom:updateDom,
+          updateGDP2:updateGDP2,
+          updateGDP3:updateGDP3,
+          updateAvg:updateAvg
+        };
 
-    this.service.updateData(updating).subscribe(
-      (data)=>{
-        console.log(data);
+        this.service.updateData(updating).subscribe(
+          (data)=>{
+            console.log(data);
+          }
+        )
+      }else{
+
+        document.getElementById("Mainerror").innerHTML = this.mainError;
+
       }
-    )
 
   }
 
